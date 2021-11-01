b13172a37ee7a6a4754ae9460fe42f3f86964b29
詠偉 高 <orzbreak@gmail.com>
2021-11-01 10:46:16 +0800

    v.1

---
+++ BSLalgorithm.cs
@@ -0,0 +1,9 @@
+if (((Differ_X >= Differ_Y) 
+            && (N == Differ_X))) {
+    pixeln = N;
+    break; 
+else if (((Differ_X < Differ_Y) 
+            && (N == Differ_Y))) {
+    pixeln = N;
+    break;
+}
